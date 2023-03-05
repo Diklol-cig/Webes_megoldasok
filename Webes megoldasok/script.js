52f6b1f966fa2f9d68721edcff762203b6614f4d
Diklol-cig <63556954+Diklol-cig@users.noreply.github.com>
2023-03-05 13:06:21 +0100

    Add files via upload

---
+++ Webes megoldasok/script.js
@@ -0,0 +1,55 @@
+const choices = document.querySelectorAll('.choice');
+
+const userChoiceSpan = document.getElementById('userChoice');
+
+const computerChoiceSpan = document.getElementById('computerChoice');
+
+const outcomeSpan = document.getElementById('outcome');
+
+const choicesArray = ['rock', 'paper', 'scissors'];
+
+let WIN = 0;
+let LOSE = 0;
+let TIE = 0;
+
+choices.forEach(choice => choice.addEventListener('click', e => {
+	userChoice = e.target.id;
+	console.log(userChoice);
+	userChoiceSpan.textContent = userChoice;
+	computerChoice = choicesArray[Math.floor(Math.random() * choicesArray.length)];
+	computerChoiceSpan.textContent = computerChoice;
+	outcome = determineOutcome(userChoice, computerChoice);
+	outcomeSpan.textContent = outcome;
+}));
+
+
+function determineOutcome(userChoice, computerChoice) {
+	if (userChoice === computerChoice) {
+		TIE++;
+		document.getElementById("TIE").innerHTML = TIE;
+
+		return "Döntetlen!";
+	} 
+	else if ((userChoice === "rock" && computerChoice === "scissors") || 
+			   (userChoice === "paper" && computerChoice === "rock") ||
+			   (userChoice === "scissors" && computerChoice === "paper")) {
+			WIN++;
+			document.getElementById("WIN").innerHTML = WIN;
+			return "Gratulálok, nyertél!";
+	} 
+	else {
+		LOSE++;
+		document.getElementById("LOSE").innerHTML = LOSE;
+		return "Vesztettél!";
+	}
+}
+
+function resetGame() {
+	userChoiceSpan.textContent = "";
+    computerChoiceSpan.textContent = "";
+    outcomeSpan.textContent = "";
+    WIN = 0;
+    LOSE = 0;
+    TIE = 0;
+}
+
